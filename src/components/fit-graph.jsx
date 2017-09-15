f846d202ccc2a97093a1b953493c3461deac5b01
scott <srveale42@gmail.com>
2017-09-15 12:31:11 -0600

    rendered graph

--- src/components/fit-graph.jsx
+++ src/components/fit-graph.jsx
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 
 class FitGraph extends Component {
   	constructor (props) {
     	super(props)
     	this.state={
-    		graphData: []
+    		graphData: [75, 70, 64, 60, 56, 52, 51, 50, 50, 50]
     	}
   	}
 
   	componentDidMount() {
-  		console.log('in component did mount')
-  		fetch('/data/artist')
-  			.then(res => {
-  				console.log('getting response')
-  				return res.json();
-  			})
-  			.then(graphData => {
-  				console.log('graphData', graphData);
-  				this.setState({ graphData });
-  			});
+  		console.log('in component did mount', this.state.graphData)
+      d3.select(".chart")
+        .selectAll("div")
+          .data(this.state.graphData)
+        .enter().append("div")
+          .style("width", function(d) { return d * 10 + "px"; })
+          .text(function(d) { return d; });
+  		// fetch('/data/artist')
+  		// 	.then(res => {
+  		// 		console.log('getting response')
+  		// 		return res.json();
+  		// 	})
+  		// 	.then(graphData => {
+  		// 		console.log('graphData', graphData);
+  		// 		this.setState({ graphData });
+  		// 	});
   	}
+    shouldComponentUpdate(props) {
+      const chart = d3.select(".chart")
+      d3.select(".chart")
+        .selectAll("div")
+          .data(this.state.graphData)
+        .enter().append("div")
+          .style("width", function(d) { return d * 10 + "px"; })
+          .text(function(d) { return d; });
+      return false
+    }
 
   	render() {
   		return (
   			<div>
-  				<p>Yeah you got here fool </p>
-  				{this.state.graphData.map(data =>
-  					<div key={data.id}>{data.username}</div>
-  				)}
+          <div className="chart">
+          </div>
+
   			</div>
   		)
   	}
