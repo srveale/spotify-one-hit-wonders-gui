502c68dda0b2d7c0cb14ca9bc07c06ed193958f1
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-15 14:52:58 -0600

    moved search into app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import './App.css';
 import FitGraph from './components/fit-graph.jsx'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>One-hit-wonders based on Spotify track popularity</h2>
-        </div>
-        <FitGraph />
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            artistData: {
+                popularities: [43, 42, 41, 40],
+            }
+        }
+
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(artistName) {
+        console.log('getting artist', artistName)
+        fetch(`/data/${artistName}`)
+            .then(res => {
+                console.log('getting response')
+                return res.json();
+            })
+            .then(artistData => {
+                console.log('popularities', artistData);
+                this.setState({ artistData });
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Spotify Artist Track Popularity</h2>
+                </div>
+                <form onSubmit={this.handleSearch}>
+                    <input type="text">
+                    </input>
+                    <button type="submit"> Get Artist Data </button>
+                </form>
+                <FitGraph handleSearch={this.handleSearch} artistData={this.state.artistData}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/fit-graph.jsx
+++ src/components/fit-graph.jsx
@@ -4,46 +4,36 @@ import * as d3 from 'd3';
 class FitGraph extends Component {
   	constructor (props) {
     	super(props)
-    	this.state={
-    		graphData: [75, 70, 64, 60, 56, 52, 51, 50, 50, 50]
-    	}
   	}
 
   	componentDidMount() {
-  		console.log('in component did mount', this.state.graphData)
-      d3.select(".chart")
-        .selectAll("div")
-          .data(this.state.graphData)
-        .enter().append("div")
-          .style("width", function(d) { return d * 10 + "px"; })
-          .text(function(d) { return d; });
-  		// fetch('/data/artist')
-  		// 	.then(res => {
-  		// 		console.log('getting response')
-  		// 		return res.json();
-  		// 	})
-  		// 	.then(graphData => {
-  		// 		console.log('graphData', graphData);
-  		// 		this.setState({ graphData });
-  		// 	});
-  	}
-    shouldComponentUpdate(props) {
-      const chart = d3.select(".chart")
-      d3.select(".chart")
-        .selectAll("div")
-          .data(this.state.graphData)
-        .enter().append("div")
-          .style("width", function(d) { return d * 10 + "px"; })
-          .text(function(d) { return d; });
-      return false
+  		console.log('in component did mount', this.props)
+  		console.log('this.props.artistData.popularites', this.props.artistData.popularities)
+  		d3.select(".chart")
+	        .selectAll("div")
+			.data(this.props.artistData.popularities)
+				.enter()
+			.append("div")
+				.style("width", function(d) { return d * 10 + "px"; })
+				.text(function(d) { return d; });
     }
 
-  	render() {
-  		return (
-  			<div>
-          <div className="chart">
-          </div>
+    shouldComponentUpdate(props) {
+      d3.select(".chart")
+        .selectAll("div")
+          .data(this.props.artistData.popularities)
+        .enter().append("div")
+          .style("width", function(d) { return d * 10 + "px"; })
+          .text(function(d) { return d; });
+    }
 
+    render() {
+      return (
+  			<div>
+         		<div className="chart">
+          		</div>
+          		<div>
+          		</div> 
   			</div>
   		)
   	}
