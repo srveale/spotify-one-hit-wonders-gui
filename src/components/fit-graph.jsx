2939ae3a48f20a14934bd111239a0c6daf5432bc
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-19 16:30:56 -0600

    safely fail if no artist data

--- src/components/fit-graph.jsx
+++ src/components/fit-graph.jsx
@@ -61,12 +61,15 @@ class FitGraph extends Component {
 
     componentWillReceiveProps(nextProps) {
         console.log('in componentWillReceiveProps', nextProps.artistData.artistName)
-        this.setState({ artistName: nextProps.artistData.artistName });
+        this.setState({ 
+            artistName: nextProps.artistData.artistName,
+            error: nextProps.artistData.error
+        });
     }
 
     componentDidUpdate(prevProps, prevState) {
         console.log('in compononentDidUpdate', this.props.artistData.popularities)
-        const popularities = this.props.artistData.popularities;
+        const popularities = this.props.artistData.popularities || [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         const svg = d3.select("svg");
 
         const margin = {top: 20, right: 20, bottom: 30, left: 40};
@@ -75,12 +78,10 @@ class FitGraph extends Component {
 
         const x = d3.scaleBand().range([0, width]).padding(0.1)
         const y = d3.scaleLinear().range([height, 0]);
-        x.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) // put tracks here
+        x.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         y.domain([0, 100])
 
-        const g = svg.select('g')
-
-        g.selectAll(".bar")
+        svg.select('g').selectAll(".bar")
             .data(popularities)
                 .attr("class", "bar")
                 .attr("x", (d, i) => x(i + 1))
@@ -90,8 +91,8 @@ class FitGraph extends Component {
     }
 
     render() {
+        const error = this.state.error;
         const artistData = this.props.artistData;
-        console.log('in render function', artistData)
         const artistName = this.state.artistName;
         const ohwFactor = artistData.fitParams ? Math.abs(artistData.fitParams.equation[1]) * 1000 : null;
         return (
@@ -101,6 +102,7 @@ class FitGraph extends Component {
           		<svg width="960" height="600" id="bar-chart">
           		</svg>
                 {artistName && <h1> Results for "<strong>{artistName}</strong>" </h1>}
+                {error && <h1> {error} </h1>}
                 {ohwFactor && <h1> One-Hit-Wonder Factor: {ohwFactor} </h1>}
   			</div>
   		)
