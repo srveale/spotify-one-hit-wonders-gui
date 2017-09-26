ae45037d7f807b8e2670200bb07149f3665eec28
scott <srveale42@gmail.com>
2017-09-25 22:01:15 -0600

    changed deploy to serve

--- scripts/serve.js
+++ scripts/serve.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 console.log('serving on port 3000')
 console.log('it totally should be working')
-console.log('filesystem', process.cwd)
+console.log('filesystem', process.cwd())
 
 
 const server = serve('../build', { port: 3000 });
