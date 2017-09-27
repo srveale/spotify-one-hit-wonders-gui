c3130c548d0180c2d74c859792ed3c80e1aabbf8
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-27 17:10:40 -0600

    deploying

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,11 +1,11 @@
 console.log('starting serve')
+console.log('it totally should be working')
+console.log('filesystem process.cwd()', process.cwd())
+console.log('__dirname', __dirname)
 const serve = require('serve');
 const fs = require('fs');
 
 console.log('serving on port 3000')
-console.log('it totally should be working')
-console.log('filesystem process.cwd()', process.cwd())
-console.log('__dirname', __dirname)
 
 
 const server = serve('./build', { port: 3000 });
